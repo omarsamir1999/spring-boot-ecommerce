0cdb30a1eececa366576b014a440f66e64980b7b
Omar Samir <omarsamir430@gmail.com>
2023-02-24 11:43:29 +0200

    add project

--- src/main/java/com/luv2code/ecommerce/entity/Order.java
+++ src/main/java/com/luv2code/ecommerce/entity/Order.java
@@ -1,15 +1,13 @@
 package com.luv2code.ecommerce.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 import javax.persistence.*;
 import java.util.List;
 
 
-    @Entity
+@Entity
     @Table(name="order")
-// @Data -- known bug
     @Getter
     @Setter
     public class Order {
@@ -17,13 +15,8 @@ import java.util.List;
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         @Column(name = "id")
         private Long id;
-
-        @Column(name = "name")
-        private String categoryName;
-
-        @Column(name = "imageUrl")
-        private String imageUrl;
-
+        @Column(name = "product")
+        List<Product> products;
         @Column(name = "quantity")
         private int quantity;
 
