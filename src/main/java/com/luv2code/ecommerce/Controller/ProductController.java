e613c227856ba5e65bc94584fc62f459db0bdfe2
Omar Samir <omarsamir430@gmail.com>
2023-02-25 12:54:30 +0200

    add project

--- src/main/java/com/luv2code/ecommerce/Controller/ProductController.java
+++ src/main/java/com/luv2code/ecommerce/Controller/ProductController.java
@@ -3,11 +3,13 @@ package com.luv2code.ecommerce.Controller;
 
 import com.luv2code.ecommerce.dao.ProductRepository;
 
+import com.luv2code.ecommerce.entity.Order;
 import com.luv2code.ecommerce.entity.Product;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -32,5 +34,10 @@ public class ProductController {
     @GetMapping("/product/subCategory/{id}")
     List<Product> findBySubCate(@PathVariable Long id) {
         return productRepository.fetchProduct(id);
+    }
+
+    @PostMapping("product/add")
+    public Product addTask(@Valid @RequestBody Product product) {
+        return productRepository.save(product);
     }
 }
